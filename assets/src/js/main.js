973f166a0e4f6636ab330312ed82353b90becb25
Ася Захарова <asya.zaharova@gmail.com>
2022-04-19 20:11:52 +0200

    Init

---
+++ assets/src/js/main.js
@@ -0,0 +1,22 @@
+// Импортируем jQuery
+//= ../../../node_modules/jquery/dist/jquery.js
+
+// Импортируем Popper
+//= ../../../node_modules/popper.js/dist/umd/popper.js
+
+// Импортируем необходимые js-файлы Bootstrap
+
+//= ../../../node_modules/bootstrap/js/dist/dom/data.js
+//= ../../../node_modules/bootstrap/js/dist/dom/event-handler.js
+//= ../../../node_modules/bootstrap/js/dist/dom/manipulator.js
+//= ../../../node_modules/bootstrap/js/dist/dom/selector-engine.js
+
+
+//= ../../../node_modules/bootstrap/js/dist/base-component.js
+//= ../../../node_modules/bootstrap/js/dist/collapse.js
+//= ../../../node_modules/bootstrap/js/dist/modal.js
+//= ../../../node_modules/bootstrap/js/dist/dropdown.js
+//= ../../../node_modules/bootstrap/js/dist/tab.js
+
+// Импортируем другие js-файлы
+//= tomat.js
---
+++ assets/src/js/tomat.js
@@ -0,0 +1,65 @@
+//JQuery Module Pattern
+"use strict";
+
+// An object literal
+const app = {
+    init() {
+        app.initTabsNavigation();
+        app.setNumbers();
+        app.imgLightbox();
+    },
+    imgLightbox() {
+        var modal = document.getElementById('lightbox')
+        modal.addEventListener('show.bs.modal', function (event) {
+            var button = event.relatedTarget
+            var src = button.getAttribute('data-bs-src')
+            var img = modal.querySelector('img')
+            img.src = src
+        })
+
+    },
+    setNumbers() {
+        var numberEl = $('.tabs-content .tab-pane:not(.fade) .number')
+        var counter = 1;
+        numberEl.each((n, el) => {
+            var number = $(el);
+            number.html(counter);
+            counter++;
+        })
+    },
+    initTabsNavigation() {
+        var tabsNavEl = $('.nav-item')
+        var tabsContentEl = $('.tab-pane')
+        tabsNavEl.click((e) => {
+            var el = $(e.currentTarget)
+            var tab = el.data('tab-target')
+
+            tabsNavEl.each((n, el) => {
+                $(el).removeClass('active')
+            })
+                el.addClass('active')
+
+                if (tab == 'all') {
+                    tabsContentEl.each((n, el) => {
+                        var t = $(el)
+                        t.removeClass('fade')
+                    })
+                } else {
+                    tabsContentEl.each((n, el) => {
+                        var t = $(el)
+                        if (t.attr('id') == tab) {
+                            t.removeClass('fade')
+                        } else {
+                            t.addClass('fade')
+                        }
+                    })
+                }
+            this.setNumbers();
+        })
+
+    }
+};
+
+$("document").ready(() => {
+    app.init();
+});
